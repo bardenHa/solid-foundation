f7c5ec42bb7d87ee647da72c126e523b9ad225c7
Harry <bardenha@gmail.com>
2023-06-22 21:57:23 +0100

    feat: add solid icons

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,6 @@
 import { createSignal } from 'solid-js';
 import { Button, HStack, Heading, VStack, Text } from '@hope-ui/solid';
+import { FiPlus, FiMinus } from 'solid-icons/fi';
 
 export default function Home() {
   const [count, setCount] = createSignal(0);
@@ -9,11 +10,13 @@ export default function Home() {
       <Heading as={'h2'}>Home</Heading>
       <Text>This is the home page.</Text>
       <HStack spacing={'$3'}>
-        <Button variant={'subtle'} onClick={() => setCount(count() - 1)}>
+        <Button variant={'subtle'} onClick={() => setCount(count() - 1)} rightIcon={<FiMinus />}>
           Decrement
         </Button>
         <output>Count: {count()}</output>
-        <Button onClick={() => setCount(count() + 1)}>Increment</Button>
+        <Button onClick={() => setCount(count() + 1)} rightIcon={<FiPlus />}>
+          Increment
+        </Button>
       </HStack>
     </VStack>
   );
